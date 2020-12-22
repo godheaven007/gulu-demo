2518a2ac06e06e9c4ec3274676cf4e7de0776e1c
godheaven007 <345777292@qq.com>
2020-12-22 17:25:25 +0800

    g-button init

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,48 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="setDirection">
+      <svg class="icon g-button-icon" aria-hidden="true" v-if="iconType">
+        <use :xlink:href=`#i-${iconType}`></use>
+      </svg>
+      <div class="g-button-txt">
+        <slot></slot>
+      </div>
+    </button>
 </template>
 
 <script>
     export default {
-
+      props: {
+        iconType: {
+          type: String
+        },
+        iconPosition: {
+          type: String,
+          default: 'left',
+          validator(value) {
+            return ['left', 'right'].indexOf(value) !== -1;
+          }
+        }
+      },
+      computed: {
+        setDirection() {
+          return this.iconPosition === 'left' ? 'g-button-left' : 'g-button-right';
+        }
+      }
     }
 </script>
 
 <style lang="scss">
+    .icon {
+      width: 1em; height: 1em;
+      vertical-align: -0.15em;
+      fill: currentColor;
+      overflow: hidden;
+    }
     .g-button {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: middle;
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
@@ -25,5 +58,22 @@
         &:focus {
             outline: none;
         }
+        &.g-button-left {
+          > .g-button-icon {
+            order: 1;
+          }
+          > .g-button-txt {
+            order: 2;
+          }
+        }
+        &.g-button-right {
+          > .g-button-icon {
+            order: 2;
+          }
+          > .g-button-txt {
+            order: 1;
+          }
+        }
+
     }
 </style>
