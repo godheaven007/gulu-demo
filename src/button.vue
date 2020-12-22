d2ab32472be8bd788b61b591165329338abe8a87
godheaven_007 <345777292@qq.com>
2020-12-22 22:06:37 +0800

    icon组件添加

--- src/button.vue
+++ src/button.vue
@@ -1,43 +1,35 @@
 <template>
-    <button class="g-button" :class="setDirection">
-      <svg class="icon g-button-icon" aria-hidden="true" v-if="iconType">
-        <use :xlink:href=`#i-${iconType}`></use>
-      </svg>
-      <div class="g-button-txt">
-        <slot></slot>
-      </div>
+    <button class="g-button" :class="{[`g-button-${iconPosition}`]: true}">
+        <g-icon :type="iconType" class="g-button-icon" v-if="iconType"></g-icon>
+        <div class="g-button-txt">
+            <slot></slot>
+        </div>
     </button>
 </template>
 
 <script>
     export default {
-      props: {
-        iconType: {
-          type: String
+        props: {
+            iconType: {
+                type: String
+            },
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return ['left', 'right'].indexOf(value) !== -1;
+                }
+            }
         },
-        iconPosition: {
-          type: String,
-          default: 'left',
-          validator(value) {
-            return ['left', 'right'].indexOf(value) !== -1;
-          }
+        computed: {
+            setDirection() {
+                return this.iconPosition === 'left' ? 'g-button-left' : 'g-button-right';
+            }
         }
-      },
-      computed: {
-        setDirection() {
-          return this.iconPosition === 'left' ? 'g-button-left' : 'g-button-right';
-        }
-      }
     }
 </script>
 
 <style lang="scss">
-    .icon {
-      width: 1em; height: 1em;
-      vertical-align: -0.15em;
-      fill: currentColor;
-      overflow: hidden;
-    }
     .g-button {
         display: inline-flex;
         align-items: center;
@@ -58,21 +50,19 @@
         &:focus {
             outline: none;
         }
-        &.g-button-left {
-          > .g-button-icon {
+        > .g-button-icon {
             order: 1;
-          }
-          > .g-button-txt {
+        }
+        > .g-button-txt {
             order: 2;
-          }
         }
         &.g-button-right {
-          > .g-button-icon {
-            order: 2;
-          }
-          > .g-button-txt {
-            order: 1;
-          }
+            > .g-button-icon {
+                order: 2;
+            }
+            > .g-button-txt {
+                order: 1;
+            }
         }
 
     }
---
+++ src/icon.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href=`#i-${type}`></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "icon",
+        props: {
+            type: {
+                // default: 'setting'
+            }
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    .icon {
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
