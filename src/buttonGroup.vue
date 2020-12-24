d76eb3cb0ff966a4a4aa263949f76d459059b17f
godheaven007 <345777292@qq.com>
2020-12-24 16:57:36 +0800

    g-button-group 组件创建

---
+++ src/buttonGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "buttonGroup",
+  mounted() {
+    for(let node of this.$el.children) {
+      let tagName = node.tagName.toLocaleLowerCase();
+      if(tagName != 'button') {
+        console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${tagName}`);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .g-button-group {
+    display: inline-flex;
+    > .g-button {
+      border-radius: 0;
+    }
+    > .g-button:first-child {border-radius:4px 0 0 4px;}
+    > .g-button:last-child {border-radius: 0 4px 4px 0;}
+    > .g-button:not(:first-child) {margin-left: -1px;}
+    > .g-button:hover {
+      z-index: 20;
+    }
+  }
+</style>
