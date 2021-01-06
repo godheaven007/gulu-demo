b4ca4d3310bfa0c0731f13735701a82a4e1e2a74
godheaven007 <345777292@qq.com>
2021-01-06 16:20:15 +0800

    Layout组件中断，回头再写

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="col" data-span="">
-
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]">
+    <div style="background-color:#bbbbbb;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -9,13 +11,36 @@ export default {
   name: "GuluCol",
   props: {
     span: {
-      type: Number,
-      default: 2
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+  $prefixClass: col-;
+  $totalPieces: 24;
+  .col {
+    width: 50%;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    border: 1px solid #f00;
+    padding: 0 10px;
+    @for $i from 1 through $totalPieces {
+      &.#{$prefixClass}#{$i} {
+        width: $i / $totalPieces * 100%;
+      }
+    }
 
+    $prefixClass: offset-;
+    @for $i from 1 through $totalPieces {
+      &.#{$prefixClass}#{$i} {
+        margin-left: $i / $totalPieces * 100%;
+      }
+    }
+  }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
     <slot></slot>
   </div>
 </template>
@@ -9,13 +9,14 @@ export default {
   name: "GuluRow",
   props: {
     gutter: {
-      type: Number,
-      default: 0
+      type: [Number, String]
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+  .row {
+    display: flex;
+  }
 </style>
